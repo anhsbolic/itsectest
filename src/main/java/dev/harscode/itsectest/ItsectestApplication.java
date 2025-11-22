b0732949a78e3c6b8ebb9bde356dd9a309be580b
Anhar Solehudin <anhsbolic@gmail.com>
2025-11-22 10:20:03 +0700

    Init Project

---
+++ src/main/java/dev/harscode/itsectest/ItsectestApplication.java
@@ -0,0 +1,13 @@
+package dev.harscode.itsectest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsectestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsectestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/harscode/itsectest/ItsectestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.harscode.itsectest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsectestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
